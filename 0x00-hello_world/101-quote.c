d2a7bd53a317c918cd513be745deaf2032e4c67d
Jacklineimali <imalijackline84@gmail.com>
2023-03-18 04:44:20 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main -main entry point
+ * Return: always 1 (success)
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
