287204525693ad01c19e4ffd5b9786bb26d00693
Jacklineimali <imalijackline84@gmail.com>
2023-03-21 06:06:24 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  *
+  * Return: void
+  */
+void print_alphabet_x10(void)
+{
+	char x;
+	int y = 0;
+
+	while (y <= 9)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+
+		_putchar('\n');
+
+		y++;
+	}
+}
